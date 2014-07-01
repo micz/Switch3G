4c2c29f2aca64456332446097f302d33c0ab202c
mic <m@micz.it>
2014-07-01 22:55:04 +0200

    description added

--- gen/it/micz/switch3g/R.java
+++ gen/it/micz/switch3g/R.java
@@ -17,8 +17,8 @@ public final class R {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
+        public static final int app_desc=0x7f040001;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
     }
     public static final class style {
         /** 
