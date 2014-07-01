bbb782be5aba6b851b69fc7ab165128e199646fd
mic <m@micz.it>
2014-07-01 22:22:33 +0200

    first import

---
+++ gen/it/micz/switch3g/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package it.micz.switch3g;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/it/micz/switch3g/MainActivity.java
@@ -0,0 +1,35 @@
+package it.micz.switch3g;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+    
+    @Override
+	public void onStart() {
+		super.onStart();
+		Intent intent = new Intent();
+		//intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+		if(currentapiVersion>=android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH){
+			//Android 4.0+
+			intent.setAction(android.provider.Settings.ACTION_DATA_ROAMING_SETTINGS);
+		}else{
+			//Other Android
+			intent.setAction(android.provider.Settings.ACTION_DATA_ROAMING_SETTINGS);
+			ComponentName cName = new ComponentName("com.android.phone","com.android.phone.Settings");
+			intent.setComponent(cName);
+		}
+		startActivity(intent);
+    }
+}
