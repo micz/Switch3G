9179a399487da4ad74282086619d00bd9450e3ea
mic <m@micz.it>
2014-07-01 22:58:41 +0200

    removed two useless imports

--- src/it/micz/switch3g/MainActivity.java
+++ src/it/micz/switch3g/MainActivity.java
@@ -4,8 +4,7 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends Activity {
 
